5b0e53c556b58b3b631f401dfecb37cff898c551
arsalanghogari <ghogari@usc.edu>
2025-07-09 20:42:46 -0700

    Updated entrtypoint.sh with correct folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 source /opt/ros/noetic/setup.bash
-[ -f /opt/barracuda-thruster-manipulator/catkin_ws/devel/setup.bash ] && source /opt/barracuda-manipulator/catkin_ws/devel/setup.bash
+[ -f /opt/barracuda-manipulator/catkin_ws/devel/setup.bash ] && source /opt/barracuda-manipulator/catkin_ws/devel/setup.bash
 exec tail -f /dev/null
+
